778b3a0bf1536534f85e1b812f7d5fdaa3e72667
Steven Bosnick <sbosnick@sympatico.ca>
2018-12-28 12:17:47 -0500

    Refactor build script

--- sel4-sys/build.rs
+++ sel4-sys/build.rs
@@ -14,20 +14,13 @@ use std::path::PathBuf;
 fn main() {
     // get the values of the needed cargo-supplied variables
     let out_dir = env::var("OUT_DIR").unwrap();
-    let arch = match env::var("CARGO_CFG_TARGET_ARCH").unwrap().as_ref() {
-        "x86" => "IA32",
-        "x86_64" => "X86_64",
-        "arm" => "AARCH32",
-        "aarch64" => "AARCH64",
-        arch => panic!("Unsupported architecture: {}", arch),
-    };
 
     // build the native sel4 library
     cmake::Config::new(".")
         .generator("Ninja")
         .define("CMAKE_TOOLCHAIN_FILE", "seL4-10.1.1/gcc.cmake")
         .define("LibSel4FunctionAttributes", "public")
-        .define(arch, "1")
+        .set_arch(Arch::from_cargo())
         .build_target("libsel4.a")
         .very_verbose(true)
         .build();
@@ -36,4 +29,54 @@ fn main() {
     let from: PathBuf = [out_dir.as_ref(), "build", "libsel4", "libsel4.a"].iter().collect();
     let to: PathBuf = [out_dir.as_ref(), "libsel4.a"].iter().collect();
     fs::copy(from, to).expect("Unable to copy libsel4.a");
+}
+
+trait CmakeExt {
+    fn set_arch(&mut self, arch: Arch) -> &mut Self;
+}
+
+impl CmakeExt for cmake::Config {
+    fn set_arch(&mut self, arch: Arch) -> &mut Self {
+        let arch: &str = arch.into();
+        self.define(arch, "1")
+    }
+}
+
+enum Arch {
+    Ia32,
+    X86_64,
+    Aarch32,
+    Aarch64,
+}
+
+impl Arch {
+    fn new(arch: impl AsRef<str>) -> Result<Arch, String> {
+        use self::Arch::*;
+
+        match arch.as_ref() {
+            "x86" => Ok(Ia32),
+            "x86_64" => Ok(X86_64),
+            "arm" => Ok(Aarch32),
+            "aarch64" => Ok(Aarch64),
+            arch => Err(format!("Unsupported architecture: {}", arch)),
+        }
+    }
+
+    fn from_cargo() -> Arch {
+        Arch::new(env::var("CARGO_CFG_TARGET_ARCH").expect("CARGO_CFG_TARGET_ARCH not set."))
+            .expect("CARGO_CFG_TARGET_ARCH not set to supported architecture.")
+    }
+}
+
+impl From<Arch> for &'static str {
+    fn from(arch : Arch) -> Self {
+        use self::Arch::*;
+
+        match arch {
+            Ia32 => "IA32",
+            X86_64 => "X86_64",
+            Aarch32 => "AARCH32",
+            Aarch64 => "AARCH64",
+        }
+    }
 }
